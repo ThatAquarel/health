71459f467f74c57845196247984c26d92fac89da
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-02-18 11:49:12 -0500

    visualize

--- data/worldbank/visualize_completeness.py
+++ data/worldbank/visualize_completeness.py
@@ -5,4 +5,18 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv("./data/worldbank/2022-2003_worldbank_data.csv")
 
-...
+YEARS = [f"{i} [YR{i}]" for i in range(2003, 2023)]
+
+for YEAR in YEARS:
+    columns = ["Country Name", "Series Name", YEAR]
+
+    current = data[columns]
+    current.loc[:, YEAR] = pd.isna(current[YEAR]) * 1
+
+    completeness = current.pivot_table(YEAR, "Country Name", "Series Name")
+
+    print(completeness.shape)
+    print(YEAR)
+
+    plt.imshow(completeness.to_numpy())
+    plt.show()
