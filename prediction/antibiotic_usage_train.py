0c9ebf343aa66187ee2855c39f6000db904b610f
ThatAquarel <xia_tianyi@outlook.com>
2024-02-20 09:32:38 -0500

    worldbank dataset formatting

--- prediction/antibiotic_usage_train.py
+++ prediction/antibiotic_usage_train.py
@@ -15,7 +15,25 @@ class AntibioticDataset(Dataset):
 
         self.train = train
 
-        worldbank = pd.read_csv(WORLDBANK)
-        antibiotics = pd.read_csv(ANTIBIOTICS)
+        self._worldbank = pd.read_csv(WORLDBANK)
+        self._prep_worldbank(2000, 2019)
+
+        self._antibiotics = pd.read_csv(ANTIBIOTICS)
+
+    def _format_worldbank_year(self, year):
+        return f"{year} [YR{year}]"
+
+    def _prep_worldbank(self, start_year, end_year): 
+        keys = ["Country Name", "Country Code", "Series Name", "Series Code"]
+        years = [self._format_worldbank_year(i) for i in range(start_year, end_year)]
+        year_key = "Year"
+
+        self._worldbank = self._worldbank[[*keys, *years]]
+        self._worldbank = pd.melt(self._worldbank, id_vars=keys, value_vars=years, var_name=year_key)
+
 
     def __len__(self): ...
+
+
+train_dataloader = DataLoader(AntibioticDataset(), batch_size=64)
+
