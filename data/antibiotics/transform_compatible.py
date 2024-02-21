b2675413a2a5855e7d11a37a46a3a041ffbdff5c
ThatAquarel <xia_tianyi@outlook.com>
2024-02-21 09:30:47 -0500

    transform antibiotic dataset

--- data/antibiotics/transform_compatible.py
+++ data/antibiotics/transform_compatible.py
@@ -6,6 +6,9 @@ raw = pd.read_csv(
 
 link = pd.read_csv("./data/antibiotics/links/Location_Country_Name.csv")
 link = link.dropna()
+link = link[["Location", "Country Name"]]
 
+raw = raw.merge(link, on="Location")
+raw = raw[["Country Name", "Year", "ATC level 3 class", "Antibiotic consumption (DDD/1,000/day)"]]
 
-...
+raw.to_csv("./data/antibiotics/2018-2000_antibiotic_atc3.csv")
