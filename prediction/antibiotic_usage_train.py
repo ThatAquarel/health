f5588152d1199259ec714b5b963ca6babbf518c8
ThatAquarel <xia_tianyi@outlook.com>
2024-02-21 19:42:06 -0500

    dataset load

--- prediction/antibiotic_usage_train.py
+++ prediction/antibiotic_usage_train.py
@@ -4,9 +4,8 @@ import pandas as pd
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 
-
-WORLDBANK = "./data/worldbank/2022-2000_worldbank_data.csv"
-ANTIBIOTICS = "./data/antibiotics/2018-2000_antiobiotic_consumption_estimates_atc3.csv"
+WORLDBANK = "./data/worldbank/2022-2000_worldbank_normalized.csv"
+ANTIBIOTICS = "./data/antibiotics/2018-2000_antibiotic_normalized.csv"
 
 
 class AntibioticDataset(Dataset):
@@ -19,25 +18,20 @@ class AntibioticDataset(Dataset):
         self._prep_worldbank(2000, 2019)
 
         self._antibiotics = pd.read_csv(ANTIBIOTICS)
+        self._prep_antiobiotics("J01C-Penicillins")
 
         ...
 
     def _prep_worldbank(self, start_year, end_year):
-        keys = ["Country Name", "Country Code", "Series Name", "Series Code"]
-        years = {f"{i} [YR{i}]": f"{i}" for i in range(start_year, end_year)}
+        lower = self._worldbank["Year"] >= start_year
+        upper = self._worldbank["Year"] < end_year
 
-        self._worldbank = self._worldbank[[*keys, *list(years.keys())]]
-        self._worldbank = self._worldbank.rename(columns=years)
+        self._worldbank = self._worldbank[lower & upper]
+        self._worldbank = self._worldbank[["Country Name", "Year", "Series Name", "Indicator"]]
 
-        var_key = "Year"
-        val_key = "Indicator"
-        self._worldbank = pd.melt(
-            self._worldbank,
-            id_vars=keys,
-            value_vars=list(years.values()),
-            var_name=var_key,
-            value_name=val_key,
-        )
+    def _prep_antiobiotics(self, atc_3_class):
+        self._antibiotics = self._antibiotics[self._antibiotics["ATC level 3 class"] == atc_3_class]
+        self._antibiotics = self._antibiotics[["Country Name", "Year", "Antibiotic consumption (DDD/1,000/day)"]]
 
     def __len__(self): ...
 
