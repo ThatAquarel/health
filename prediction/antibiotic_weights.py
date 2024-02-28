a74b374795d108bf864ff48f67bd950ec5d060c6
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-02-28 18:38:14 -0500

    weights analysis

--- prediction/antibiotic_weights.py
+++ prediction/antibiotic_weights.py
@@ -6,3 +6,7 @@ from antibiotic_usage_train import AntibioticPredictor, MODEL
 model = AntibioticPredictor()
 model.load_state_dict(torch.load(MODEL))
 model.eval()
+
+for name, param in model.named_parameters():
+    print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
+...
