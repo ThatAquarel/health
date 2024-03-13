645c9ac131c85c368d435b21278ecfddb3409a5b
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-03-13 19:46:48 -0400

    ordered factors

--- prediction/integrated_gradients.py
+++ prediction/integrated_gradients.py
@@ -19,42 +19,43 @@ baseline = torch.zeros(db_x_test.shape)
 ...
 
 ig = IntegratedGradients(model)
-attributions, approximation_error = ig.attribute(
-    db_x_test,
-    baselines=baseline,
-    method="gausslegendre",
-    return_convergence_delta=True,
-    target=4,
-)
 
+for level, label in {0: "low", 4: "high"}.items():
+    attributions, approximation_error = ig.attribute(
+        db_x_test,
+        baselines=baseline,
+        method="gausslegendre",
+        return_convergence_delta=True,
+        target=level,
+    )
 
-def visualize_importances(
-    feature_names,
-    importances,
-    title="Average Feature Importances",
-    plot=True,
-    axis_title="Features",
-):
-    print(title)
-    for i in range(len(feature_names)):
-        print(feature_names[i], ": ", "%.3f" % (importances[i]))
-    x_pos = np.arange(len(feature_names))
-    if plot:
-        plt.figure(figsize=(12, 6))
-        plt.bar(x_pos, importances, align="center")
-        plt.xticks(x_pos, feature_names, wrap=True)
-        plt.xlabel(axis_title)
-        plt.title(title)
+    importance = np.mean(attributions.cpu().numpy(), axis=0)
 
+    indices = np.argsort(importance)
+    factors = pd.read_csv("./data/worldbank/links/Series_Name_Series_Code.csv")
+    factors = factors[["Series Name", "Series Code"]]
 
-importance = np.mean(attributions.cpu().numpy(), axis=0)
+    factors.insert(2, "Attribution", list(importance), True)
 
+    ordered = factors.reindex(indices)
+    ordered.to_csv(f"./prediction/results/ordered_factors_2018_{label}.csv")
+
+# def visualize_importances(
+#     feature_names,
+#     importances,
+#     title="Average Feature Importances",
+#     plot=True,
+#     axis_title="Features",
+# ):
+#     print(title)
+#     for i in range(len(feature_names)):
+#         print(feature_names[i], ": ", "%.3f" % (importances[i]))
+#     x_pos = np.arange(len(feature_names))
+#     if plot:
+#         plt.figure(figsize=(12, 6))
+#         plt.bar(x_pos, importances, align="center")
+#         plt.xticks(x_pos, feature_names, wrap=True)
+#         plt.xlabel(axis_title)
+#         plt.title(title)
 # visualize_importances(np.arange(1683), importance, axis=0)
 # plt.show()
-
-indices = np.argsort(importance)
-factors = pd.read_csv("./data/worldbank/links/Series_Name_Series_Code.csv")
-factors = factors[["Series Name", "Series Code"]]
-
-ordered = factors.reindex(indices)
-ordered.to_csv("./prediction/results/ordered_factors.csv")
