c9d3d2428e9c2b93c4d103000a2ca3beddf7483b
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-03-15 16:55:26 -0400

    antibiotic usage data

--- data/antibiotics/normalize.py
+++ data/antibiotics/normalize.py
@@ -7,13 +7,11 @@ ATC3 = "ATC level 3 class"
 CONSUMPTION = "Antibiotic consumption (DDD/1,000/day)"
 
 antibiotics = pd.read_csv(ANTIBIOTICS)
-antibiotics = antibiotics[
-    ["Country Name", 
-    "Year", ATC3, CONSUMPTION]
-]
+antibiotics = antibiotics[["Country Name", "Year", ATC3, CONSUMPTION]]
 
 for drug in tqdm(antibiotics[ATC3].drop_duplicates()):
     x = antibiotics.loc[antibiotics[ATC3] == drug, CONSUMPTION]
-    antibiotics.loc[antibiotics[ATC3] == drug, CONSUMPTION] = (x-x.mean())/x.std()
+    print(f"{x.mean()} {x.std()}")
+    antibiotics.loc[antibiotics[ATC3] == drug, CONSUMPTION] = (x - x.mean()) / x.std()
 
 antibiotics.to_csv("./data/antibiotics/2018-2000_antibiotic_normalized.csv")
