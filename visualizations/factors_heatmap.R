8ced6367bb87c00b91c015f1da4c089767bfc26c
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-04-01 12:19:59 -0400

    abr levels annotation

--- visualizations/factors_heatmap.R
+++ visualizations/factors_heatmap.R
@@ -8,46 +8,39 @@ col_filter <- !(names(categorized_factors) %in% drop)
 country_attributions = categorized_factors[col_filter]
 
 # build matrix
-factors <- categorized_factors[["Series.Name"]]
-countries <- names(categorized_factors)[col_filter]
-
 mat_country_attributions <- data.matrix(country_attributions)
-#rownames(mat_country_attributions) = factors
-#colnames(mat_country_attributions) = countries
 
 # categorize factors
 factors_categories <- categorized_factors[["Category"]]
 
 # categorize countries
 categorized_countries <- read.csv("./prediction/results/predicted_categories.csv")
-categories <- c("Low", "Medium-low", "Medium", "Medium-high", "High")
-countries_categories <- categories[categorized_countries[["Predicted.Category"]] + 1]
+countries_categories <- categorized_countries[["Predicted.Category"]]
 
 # color function
 library(circlize)
 col_fun_heatmap <- colorRamp2(c(-0.2, 0, 0.05), c("blue", "white", "red"))
 
 # ABR risk level annotation (columns)
-
 abr_ha <- HeatmapAnnotation(
-  ABR_risk = factor(countries_categories, levels=categories),
+  ABR_risk = countries_categories,
   annotation_legend_param = list(
-    ABR_risk = list(ABR_risk_color_bar = "discrete"))
+    ABR_risk = list(ABR_risk_color_bar = "discrete", labels=c("Low", "Medium-low", "Medium", "Medium-high", "High"))
+  )
 )
+
+# Factor category annotation (rows)
+
 
 # draw heat map
 Heatmap(
   mat_country_attributions,
   name="factor_importance",
 
-  #row_split=factors_categories,
-  column_split=factor(countries_categories, levels=categories),
+  column_split=countries_categories,
   column_title=NULL,
   cluster_column_slices=FALSE,
-  border = TRUE,
-  
   top_annotation = abr_ha,
-  heatmap_legend_param = list(ABR_risk_color_bar = "discrete"),
 
   col=col_fun_heatmap
 )
