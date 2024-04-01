0d699f0f283cd935d84b7555e318a5642eb8b2bf
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-04-01 11:02:39 -0400

    base heatmap

--- visualizations/factors_heatmap.R
+++ visualizations/factors_heatmap.R
@@ -4,5 +4,19 @@ setwd("C:/Users/xia_t/Desktop/Projects/health")
 categorized_factors <- read.csv("./prediction/results/ordered_factors_2003_2022_high_countries.csv")
 
 drop <- c("X", "Category", "Series.Name", "Series.Code")
-country_attributions= categorized_factors[!(names(categorized_factors) %in% drop)]
+col_filter <- !(names(categorized_factors) %in% drop)
+country_attributions = categorized_factors[col_filter]
 
+# build matrix
+factors <- categorized_factors[["Series.Name"]]
+countries <- names(categorized_factors)[col_filter]
+
+mat_country_attributions <- data.matrix(country_attributions)
+#rownames(mat_country_attributions) = factors
+#colnames(mat_country_attributions) = countries
+
+# categorize factors
+factors_categories <- categorized_factors[["Category"]]
+
+# draw heat map
+Heatmap(mat_country_attributions, row_split=factors_categories)
