b71e8939ea4af6b6516ba4cd53932df5bb8b1de1
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-04-05 21:38:28 -0400

    filter by series name

--- data/worldbank/filter_factors.py
+++ data/worldbank/filter_factors.py
@@ -3,14 +3,43 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+data = pd.read_csv("./data/worldbank/2022-2000_worldbank_data.csv")
+YEARS = [f"{i} [YR{i}]" for i in range(2003, 2023)]
 
-#
+data = data.copy()
+data = data[["Country Name", "Series Name", *YEARS]]
+data.loc[:, YEARS] = pd.isna(data[YEARS])
+
 a = data.copy()
-a = a[["Country Name", "Series Name", *YEARS]]
-a.loc[:, YEARS] = pd.isna(a[YEARS])
 a = a.groupby(by=["Series Name"]).sum()
-b = pd.melt(a.reset_index(), id_vars=["Series Name"], value_vars=YEARS, var_name="Year")
-counts = b.groupby(by=["Series Name"]).sum()
+a = pd.melt(a.reset_index(), id_vars=["Series Name"], value_vars=YEARS, var_name="Year")
+counts = a.groupby(by=["Series Name"]).sum()
+counts.to_csv("./data/worldbank/filtering/counts_groupby_series_name.csv")
+
+filtered = counts.loc[counts["value"] <= 2000]
+filtered = filtered.reset_index()
+
+b = data.copy()
+b = b.groupby(by=["Country Name"])
 
 ...
-counts.loc[counts["value"] <= 2000]
+
+# latest_year = data[["Country Name", "Series Name", *YEARS]].copy()
+# latest_year.loc[:, YEARS] = pd.isna(latest_year[YEARS])
+# latest_year["Counts"] = latest_year[YEARS].sum(axis=1)
+
+# first filtered stage
+#
+# filtered1 = latest_year.merge(
+#     filtered[["Series Name"]], how="inner", on=["Series Name"]
+# )
+# pivoted1 = pd.pivot_table(
+#     filtered1, values=["Counts"], index=["Series Name"], columns=["Country Name"]
+# )
+
+# sns.set_theme(rc={"figure.figsize": (24, 16)})
+# sns.clustermap(pivoted1.to_numpy())
+# plt.show()
+
+
+...
