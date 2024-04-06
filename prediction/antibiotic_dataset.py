52068d8fe5e793f2bd27529cad51f4cd1d3cbc8b
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-04-06 10:27:31 -0400

    generate y

--- prediction/antibiotic_dataset.py
+++ prediction/antibiotic_dataset.py
@@ -1,17 +1,56 @@
 import torch
 import itertools
 
+import numpy as np
 import pandas as pd
 
-ANTIBIOTICS = "./data/antibiotics/2018-2000_antibiotic_normalized.csv"
+# worldbank dataset
+worldbank = pd.read_csv("./data/worldbank/2022-2003_worldbank_normalized.csv")
+worldbank = worldbank[["Country Name", "Series Name", "Year", "Indicator"]]
+worldbank.loc[:, ["Indicator"]] = worldbank["Indicator"].astype("float32")
+worldbank = worldbank.fillna(0)
 
-
-N_INDICATOR = 1683
-
-
-worldbank = pd.read_csv("./data/worldbank/2022-2003_worldbank_filtered.csv")
+# antibiotics dataset
 antibiotics = pd.read_csv("./data/antibiotics/2018-2000_antibiotic_normalized.csv")
+CONSUMPTION = "Antibiotic consumption (DDD/1,000/day)"
+antibiotics = antibiotics[["Country Name", "Year", CONSUMPTION]]
 
+# equal width binning into N_BINS categories
+N_BINS = 5
+bins = list(range(N_BINS))
+antibiotics.loc[:, CONSUMPTION] = pd.cut(antibiotics[CONSUMPTION], N_BINS, labels=bins)
+_, counts = np.unique(antibiotics[CONSUMPTION], return_counts=True)
+print(f"class_weights: {counts.sum() / counts}")
+
+# merge worldbank and antibiotics (2003-2018)
+merged = antibiotics.merge(worldbank, how="inner", on=["Country Name", "Year"])
+index = merged[["Country Name", "Year"]].drop_duplicates()
+index = index.reset_index(drop=True)
+N_CASES = len(index)
+N_INDICATORS = len(merged[["Series Name"]].drop_duplicates())
+
+# one hot encoding for categories
+ONE_HOT_LUT = torch.zeros((N_BINS,N_BINS)).float()
+ONE_HOT_LUT[bins, bins] = 1.0
+
+# output var (2003-2018)
+y = torch.zeros((N_CASES, N_BINS))
+y_idx = antibiotics.merge(index, how="inner", on=["Country Name", "Year"])[CONSUMPTION]
+y[range(N_CASES), y_idx] = 1.0
+assert (y.argmax(axis=1).numpy() == y_idx.to_numpy()).all()
+
+# save output var
+def get_y_idx(start_year, end_year):
+    condition = (index["Year"] >= start_year) & (index["Year"] <= end_year)
+    return index[condition].index.values
+
+# y_2003-2017_train.pt
+torch.save(y[get_y_idx(2003, 2017), :], "./prediction/y_2003-2017_train.pt")
+
+# y_2018_test.pt
+torch.save(y[get_y_idx(2018, 2018), :], "./prediction/y_2018_test.pt")
+
+...
 
 class AntibioticDataset(Dataset):
     def __init__(self, train=True, end_year=2019) -> None:
