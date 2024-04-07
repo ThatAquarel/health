1816b4c3b3fd70465e9f0e0b2bd2b262c9cd107d
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-04-07 08:27:48 -0400

    rectify antibiotic_dataset.py

--- prediction/antibiotic_dataset.py
+++ prediction/antibiotic_dataset.py
@@ -5,79 +5,76 @@ import pandas as pd
 
 torch.set_default_device("cuda:0")
 
+CASES_IDENTIFIERS = ["Country Name", "Year"]
+
 # worldbank dataset
 worldbank = pd.read_csv("./data/worldbank/2022-2003_worldbank_normalized.csv")
-worldbank = worldbank[["Country Name", "Series Name", "Year", "Indicator"]]
+worldbank = worldbank[[*CASES_IDENTIFIERS, "Series Name", "Indicator"]]
 worldbank.loc[:, ["Indicator"]] = worldbank["Indicator"].astype("float32")
 worldbank = worldbank.fillna(0)
 
 # antibiotics dataset
 antibiotics = pd.read_csv("./data/antibiotics/2018-2000_antibiotic_normalized.csv")
 CONSUMPTION = "Antibiotic consumption (DDD/1,000/day)"
-antibiotics = antibiotics[["Country Name", "Year", CONSUMPTION]]
+antibiotics = antibiotics[[*CASES_IDENTIFIERS, CONSUMPTION]]
 
 # equal width binning into N_BINS categories
 N_BINS = 5
 bins = list(range(N_BINS))
-antibiotics.loc[:, CONSUMPTION] = pd.cut(antibiotics[CONSUMPTION], N_BINS, labels=bins)
-_, counts = np.unique(antibiotics[CONSUMPTION], return_counts=True)
+antibiotics.loc[:, "Category"] = pd.cut(antibiotics[CONSUMPTION], N_BINS, labels=bins)
+antibiotics = antibiotics[[*CASES_IDENTIFIERS, "Category"]]
+_, counts = np.unique(antibiotics["Category"], return_counts=True)
 print(f"class_weights: {counts.sum() / counts}")
 
-# merge worldbank and antibiotics (2003-2018)
-merged = antibiotics.merge(worldbank, how="inner", on=["Country Name", "Year"])
-y_cases = merged[["Country Name", "Year"]].drop_duplicates()
-y_cases = y_cases.reset_index(drop=True)
-N_CASES = len(y_cases)
-N_INDICATORS = len(merged[["Series Name"]].drop_duplicates())
+# merge antibiotics and worldbank
+pivoted_worldbank = pd.pivot_table(
+    worldbank,
+    values="Indicator",
+    index=CASES_IDENTIFIERS,
+    columns=["Series Name"],
+)
+merged = pivoted_worldbank.merge(antibiotics, how="left", on=CASES_IDENTIFIERS)
+merged = merged.fillna(0)
 
-# one hot encoding for categories
-ONE_HOT_LUT = torch.zeros((N_BINS, N_BINS)).float()
-ONE_HOT_LUT[bins, bins] = 1.0
+# determine cases and axes
+unique_cases = merged[CASES_IDENTIFIERS].drop_duplicates()
+N_CASES = len(unique_cases)
 
-# output var (2003-2018)
-y = torch.zeros((N_CASES, N_BINS))
-y_idx = antibiotics.merge(y_cases, how="inner", on=["Country Name", "Year"])[
-    CONSUMPTION
-]
-y[range(N_CASES), y_idx] = 1.0
-assert (y.argmax(axis=1).cpu().numpy() == y_idx.to_numpy()).all()
+unique_indicators = merged.columns.values[2:-1]
+N_INDICATORS = len(unique_indicators)
 
 
+# build matrices
 def get_idx(cases, start_year, end_year):
     condition = (cases["Year"] >= start_year) & (cases["Year"] <= end_year)
     return cases[condition].index.values
 
 
+# build output variable
+ONE_HOT_LUT = torch.zeros((N_BINS, N_BINS)).float()
+ONE_HOT_LUT[bins, bins] = 1.0
+
+y = torch.zeros((N_CASES, N_BINS))
+y_idx = merged["Category"]
+y[range(N_CASES), y_idx] = 1.0
+assert (y.argmax(axis=1).cpu().numpy() == y_idx.to_numpy()).all()
+
 # y_2003-2017_train.pt
-torch.save(y[get_idx(y_cases, 2003, 2017), :], "./prediction/y_2003-2017_train.pt")
+torch.save(y[get_idx(unique_cases, 2003, 2017), :], "./prediction/y_2003-2017_train.pt")
 
 # y_2018_test.pt
-torch.save(y[get_idx(y_cases, 2018, 2018), :], "./prediction/y_2018_test.pt")
+torch.save(y[get_idx(unique_cases, 2018, 2018), :], "./prediction/y_2018_test.pt")
 
-# input var (2003-2022)
-x_cases = worldbank[["Country Name", "Year"]].drop_duplicates()
-x_cases = x_cases.reset_index(drop=True)
-N_CASES = len(x_cases)
 
-pivoted_worldbank = pd.pivot_table(
-    worldbank,
-    values="Indicator",
-    index=["Country Name", "Year"],
-    columns=["Series Name"],
-)
-pivoted_worldbank = pivoted_worldbank.merge(
-    x_cases, how="inner", on=["Country Name", "Year"]
-)
-x = torch.tensor(
-    pivoted_worldbank[pivoted_worldbank.columns.values[2:]].to_numpy()
-).float()
+# build input variable
+x = torch.tensor(merged[merged.columns.values[2:-1]].to_numpy()).float()
 
 # x_2003-2017_train.pt
-torch.save(x[get_idx(x_cases, 2003, 2017), :], "./prediction/x_2003-2017_train.pt")
+torch.save(x[get_idx(unique_cases, 2003, 2017), :], "./prediction/x_2003-2017_train.pt")
 
 # x_2018_test.pt
-torch.save(x[get_idx(x_cases, 2018, 2018), :], "./prediction/x_2018_test.pt")
+torch.save(x[get_idx(unique_cases, 2018, 2018), :], "./prediction/x_2018_test.pt")
 
 # x_2003-2022_infer.pt and x_2003-2022_infer_cases.csv
-torch.save(x[get_idx(x_cases, 2003, 2022), :], "./prediction/x_2003-2022_infer.pt")
-x_cases.to_csv("./prediction/x_2003-2022_infer_cases.csv")
+torch.save(x[get_idx(unique_cases, 2003, 2022), :], "./prediction/x_2003-2022_infer.pt")
+unique_cases.to_csv("./prediction/x_2003-2022_infer_cases.csv")
