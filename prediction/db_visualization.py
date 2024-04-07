39ec6adc0f99a8f4b936a0cdd85db75f5e879bfb
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-04-07 09:16:57 -0400

    rectify db_visualization.py

--- prediction/db_visualization.py
+++ prediction/db_visualization.py
@@ -1,7 +1,29 @@
 import torch
-import pandas as pd
 
-db_x_infer = torch.load("./prediction/db_x_infer.pt")
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-db_x_infer_cases = pd.read_csv("./prediction/db_x_infer_cases.csv")
-db_x_infer_cases = db_x_infer_cases[["Country Name", "Year"]]
+x = torch.load("./prediction/x_2018_test.pt")
+x = x.cpu()
+y = torch.load("./prediction/y_2018_test.pt")
+y = y.argmax(1).cpu()
+
+y, idx = y.sort()
+x = x[idx]
+
+pal = sns.husl_palette(5, s=0.45)
+y_colors = [pal[int(i)] for i in y]
+
+sns.set_theme()
+
+g = sns.clustermap(
+    x,
+    center=0,
+    cmap="vlag",
+    row_colors=y_colors,
+    row_cluster=False,
+    cbar_pos=(0.02, 0.32, 0.03, 0.2),
+    figsize=(12, 13),
+)
+
+plt.show()
