bff2c11f859c52a8feb4685db6765dc6cbc456c5
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-04-06 21:04:48 -0400

    temporary regions

--- data/worldbank/link_tables.py
+++ data/worldbank/link_tables.py
@@ -24,3 +24,18 @@ for category in series_categories["Category"].drop_duplicates():
     series_categories.loc[series_categories["Category"] == category].to_csv(
         f"./data/worldbank/links/category/{category}.csv"
     )
+
+# used Country_Name
+a = pd.read_csv("data/antibiotics/links/Location_Country_Name.csv")
+a = a[["Country Name"]].drop_duplicates()
+
+b = pd.read_csv("data/antibiotics/2018-2000_antibiotic_total.csv")
+b = b[["Country Name"]].drop_duplicates()
+
+c = pd.read_csv("data/worldbank/links/data/Country_Name_Country_Code.csv")
+c = b[["Country Name"]].drop_duplicates()
+
+c = a.merge(b, how="inner", on=["Country Name"]).merge(
+    c, how="inner", on=["Country Name"]
+)
+c.to_csv("data/worldbank/links/Country_Name.csv")
